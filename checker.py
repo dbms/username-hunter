eaccc7ec032fc7fc6e655e2dfa98f215494466d4
Deepak Balhara <ideepakbalhara@gmail.com>
2018-12-28 16:13:19 +0530

    comments added

--- checker.py
+++ checker.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 myfile = open('wordlist-wiki').read().split()
 for i in myfile:
     try:
-        url = str("https://www.instagram.com/" + i)
+        url = str("https://www.codechef.com/users/" + i) # enumeration of usernames
         print(url)
         user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
         headers = {'User-Agent': user_agent}
@@ -13,10 +13,10 @@ for i in myfile:
         soup = BeautifulSoup(html , 'html.parser')
         page_title = str(soup.title.string)
         print(page_title)
-        if "Page Not Found" in page_title:
-            f = open("instagram" , 'a')
+        if "Error Page" in page_title: # see the error message in title
+            f = open("avaiable-usernames" , 'a')
             f.write(i + "\n")
     
     except :
-        f = open("insta-error" , 'a')
+        f = open("errors" , 'a')
         f.write(i + "\n")
